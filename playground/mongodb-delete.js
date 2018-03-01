668ec3ef242df447df996f5aca5f62d50fdaedeb
ashley woodruff <ashley.woodruff@gmail.com>
2018-03-01 13:16:31 -0600

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server')
+    const db = client.db('Todoapp')
+
+    // deleteMany
+    // db.collection('Users').deleteMany({
+    //     name: 'Ashley'
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    //deleteOne
+    db.collection('Users').deleteOne({
+        _id: ObjectID('5a8f9acc99c8344e56e34fc0')
+    }).then((resuslt) => {
+        consosle.log(result);
+    });
+
+    // findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({
+    //     completed: false
+    // }).then((result) => {
+    //     conssole.log(result);
+    // });
+
+    //client.close();
+});
